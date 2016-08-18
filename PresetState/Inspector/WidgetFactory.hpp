54d99ec7c00fb3d7cd68b087ff53d0a1f65d3d85
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-08-18 14:43:15 +0200

    simplify

--- PresetState/Inspector/WidgetFactory.hpp
+++ PresetState/Inspector/WidgetFactory.hpp
@@ -1,25 +1,16 @@
 #pragma once
 #include <Process/Inspector/ProcessInspectorWidgetDelegateFactory.hpp>
-#include <PresetState/Metadata.hpp>
+#include <PresetState/Model.hpp>
+#include <PresetState/Inspector/Widget.hpp>
 
 namespace PresetState
 {
 namespace Inspector
 {
 class Factory final :
-        public Process::StateProcessInspectorWidgetDelegateFactory
+        public Process::StateProcessInspectorWidgetDelegateFactory_T<Model, Widget>
 {
         ISCORE_CONCRETE_FACTORY("577bc362-e279-488c-b0ac-5972851ea291")
-    public:
-        Factory();
-        virtual ~Factory();
-
-    private:
-        Process::StateProcessInspectorWidgetDelegate* make(
-                const Process::StateProcess&,
-                const iscore::DocumentContext&,
-                QWidget* parent) const override;
-        bool matches(const Process::StateProcess&) const override;
 };
 }
 }
